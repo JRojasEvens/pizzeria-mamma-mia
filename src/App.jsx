0f18bc4124bbf40de66fb6986713f1f1451873ed
Jorge Rojas Evens <jrojasevens@outlook.com>
2026-05-26 16:21:43 -0400

    Desafio 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Home />
+      {/* <Home /> */}
+      {/* <RegisterPage /> */}
+      <LoginPage />
       <Footer />
     </>
   );
